cea6494390337144ee190cc9c8f4f9ef4f401a6a
Jacob Magnusson <m@jacobian.se>
2012-11-13 13:48:15 +0100

    Use package_data instead of MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,31 +4,6 @@ import os
 import sys
 
 
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
-
 appname = 'grappelli_modeltranslation'
 version = __import__(appname).__version__
 
@@ -45,13 +20,13 @@ setup(
     version=version,
     description="A small compatibility layer between grappelli and " \
                 "django-modeltranslation",
-    long_description=open('README.md').read(),
     author='Jacob Magnusson',
     author_email='m@jacobian.se',
     url='https://github.com/jmagnusson/grappelli-modeltranslation',
     license='New BSD License',
     platforms=['any'],
-    packages=get_packages(appname),
+    packages=[appname],
+    package_data={appname: ['static/modeltranslation/*/*']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
